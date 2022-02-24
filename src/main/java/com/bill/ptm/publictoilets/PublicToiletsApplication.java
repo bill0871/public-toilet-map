bf2860d805f86597db2a89c92c68fb17527c55a8
Bill <pengbill22@gmail.com>
2022-02-24 22:52:14 +0800

    Initial project

---
+++ src/main/java/com/bill/ptm/publictoilets/PublicToiletsApplication.java
@@ -0,0 +1,13 @@
+package com.bill.ptm.publictoilets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicToiletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicToiletsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bill/ptm/publictoilets/PublicToiletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.ptm.publictoilets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicToiletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
